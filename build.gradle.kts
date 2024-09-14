da321879a6459be5475c4e885f0fe37195332c37
Dmitry Fedotov <dm.fe@yandex.ru>
2024-09-15 00:45:57 +0300

    Sample app basic authentication demo. Authentication diagrams.

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "net.dmfe"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/net/dmfe/SandboxSecurityApplication.java
@@ -0,0 +1,13 @@
+package net.dmfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxSecurityApplication.class, args);
+    }
+
+}
